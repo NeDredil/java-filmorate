ed9eaf9585eda557d770348888cb8d45226d7c9a
Денис Солодовников <nedredil@yandex.ru>
2023-07-20 23:14:09 +0300

    feat: Реализация ТЗ-9

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,24 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@Data
+public class Film {
+
+    private long id;
+    @NotBlank(message = "Название не может быть пустым.")
+    private final String name;
+    @Size(max = 200, message = "Максимальная длина описания — 200 символов.")
+    private final String description;
+    @NotNull
+    private final LocalDate releaseDate;
+    @NotNull
+    @Positive(message = "Продолжительность фильма должна быть положительной.")
+    private final int duration;
+}
